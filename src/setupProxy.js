5b5080ca8cc9a69113ddc421b280c446330249bf
Aiman Najjar <aiman.najjar@hurranet.com>
2021-01-27 12:32:58 -0500

    cleaner approach for containerized env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/jawhar',
     createProxyMiddleware({
-      target: 'http://172.18.0.1:5050',
+      target: 'http://host.docker.internal:5050',
       changeOrigin: true,
       pathRewrite: {
         "^/jawhar": "",

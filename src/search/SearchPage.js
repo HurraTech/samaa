5da46c16702d6f508e23f881ade15df1ab5018e2
Aiman Najjar <aiman@hurracloud.io>
2024-08-26 19:57:16 -0400

    wip: fixing search ui

--- src/search/SearchPage.js
+++ src/search/SearchPage.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useRef, useEffect, useState, useReducer} from 'react'
 import PropTypes from 'prop-types';
 import Paper from '@mui/material/Paper';
-import { withStyles } from '@mui/styles';
+import { styled } from '@mui/material/styles';
 import axios from 'axios';
 import QuickPreview from '../components/QuickPreview';
 import FilePreview from '../components/FilePreview';
@@ -12,8 +12,54 @@ import { JAWHAR_API  } from '../constants';
 import {FormControl, InputLabel, Input, FormHelperText} from '@mui/material';
 import { Typography, Select, MenuItem, List, ListItem } from '@mui/material'
 import withRoute from "../withRoute";
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const SIZE = 30;
+const PREFIX = 'SearchPage';
+const classes = {
+  filterPaper: `${PREFIX}-filterPaper`,
+  heading: `${PREFIX}-heading`,
+  paper: `${PREFIX}-paper`,
+  searchBar: `${PREFIX}-searchBar`,
+  searchInput: `${PREFIX}-searchInput`,
+  block: `${PREFIX}-block`,
+  addUser: `${PREFIX}-addUser`,
+  contentWrapper: `${PREFIX}-contentWrapper`,
+}
+
+const Root = styled('div')(({ theme }) => ({
+  [`&.${classes.filterPaper}`]: {
+    padding: "10px"
+  },
+  [`&.${classes.heading}`]: {
+    fontSize: theme.typography.pxToRem(18),
+    color: theme.palette.text.secondary,
+    width: "80px"
+  },
+  [`&.${classes.paper}`]: {
+    maxWidth: '100%',
+    margin: 'auto',
+  },
+  [`&.${classes.searchBar}`]: {
+    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+  },
+  [`&.${classes.searchInput}`]: {
+    fontSize: theme.typography.fontSize,
+  },
+  [`&.${classes.block}`]: {
+    display: 'block',
+  },
+  [`&.${classes.addUser}`]: {
+    marginRight: theme.spacing.unit,
+  },
+  [`&.${classes.contentWrapper}`]: {
+    margin: '0px 0px',
+    height: "80vh",
+    flex: 1,
+  },
+
+}))
+
 
 const styles = theme => ({
   filterPaper: {
@@ -49,11 +95,34 @@ const styles = theme => ({
   },
 });
 
-class Content extends React.Component {
-  constructor(props) {
-    super(props);
+// class Content extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
+//     state = {
+//       error: null,
+//       isInstantSearchEnabled: false,
+//       isPreviewOpen: false,
+//       isLoaded: false,
+//       previewedContent: [],
+//       openedFile: '',
+//       isInlineViewerOpen: false,
+//       previewedTitle: '',
+//       isAjaxInProgress: false,
+//       items: [],
+//       totalResults: 1000,
+//     };
+//   }
+
+const Content = (props) => {
+
+  const navigate = useNavigate()
+  const location = useLocation()
+
+
+  const [state, setState] = useReducer(
+    (state, newState) => ({...state, ...newState}),
+    {
       error: null,
       isInstantSearchEnabled: false,
       isPreviewOpen: false,
@@ -65,20 +134,21 @@ class Content extends React.Component {
       isAjaxInProgress: false,
       items: [],
       totalResults: 1000,
-    };
-  }
+    }
+  )
+  
 
-  handlePreviewClick = index => {
+  const handlePreviewClick = index => {
     const highlighted_content =
-      this.state.items[index].highlight &&
-      this.state.items[index].highlight.content
-        ? this.state.items[index].highlight.content
+      state.items[index].highlight &&
+      state.items[index].highlight.content
+        ? state.items[index].highlight.content
         : null;
 
     const full_content =
-      this.state.items[index]._source.content &&
-      this.state.items[index]._source.content.trim() != ''
-        ? [this.state.items[index]._source.content.trim()]
+      state.items[index]._source.content &&
+      state.items[index]._source.content.trim() != ''
+        ? [state.items[index]._source.content.trim()]
         : null;
     const preview_content = highlighted_content || full_content;
 
@@ -86,29 +156,40 @@ class Content extends React.Component {
       this.setState({
         isPreviewOpen: true,
         isInlineViewerOpen: false,
-        openedFile: this.state.items[index]._source.path,
+        openedFile: state.items[index]._source.path,
         previewedContent: preview_content,
-        previewedTitle: this.state.items[index]._source.file.filename,
+        previewedTitle: state.items[index]._source.file.filename,
       });
     }
   };
 
-  handleFilenameClick = index => {
-    const path = this.state.items[index].Path;
-    this.props.history.push({ pathname: `/search/${this.props.selectSourceType}/${this.props.selectSourceID}/preview${path}`, search: this.props.location.search});
+  const handleFilenameClick = index => {
+    const path = state.items[index].Path;
+    navigate({ pathname: `/search/${this.props.selectSourceType}/${this.props.selectSourceID}/preview${path}`, search: this.props.location.search});
   };
 
-
-  componentDidUpdate(prevProps) {
-    const prevQuery = this.query(prevProps);
-    const newQuery = this.query()
-
-    if (prevQuery  !== newQuery || this.props.selectSourceType !== prevProps.selectSourceType || this.props.selectSourceID !== prevProps.selectSourceID ) {
-      this.searchWrapper(newQuery, this.search);
+  const didMountRef = useRef(false)
+  useEffect(() => {
+    if (didMountRef.current) {
+    } else {
+      didMountRef.current = true
+      searchWrapper(query(), search);
     }
-  }
+  })
 
-  handlePreviewCloseClick() {
+
+  // componentDidUpdate(prevProps) {
+  //   const prevQuery = this.query(prevProps);
+  //   const newQuery = this.query()
+
+  //   if (prevQuery  !== newQuery || this.props.selectSourceType !== prevProps.selectSourceType || this.props.selectSourceID !== prevProps.selectSourceID ) {
+  //     this.searchWrapper(newQuery, this.search);
+  //   }
+  // }
+
+
+  
+  const handlePreviewCloseClick = () => {
     this.setState({
       isPreviewOpen: false,
       isInlineViewerOpen: false,
@@ -116,102 +197,92 @@ class Content extends React.Component {
     });
   }
 
-  searchWrapper = (query, searchFunction) => {
-    this.setState(
-      {
-        q: query,
-        isPreviewOpen: false,
-        isAjaxInProgress: true,
-        isInlineViewerOpen: false,
-        items: [],
-        totalResults: 0,
-        openedFile: '',
-      },
-      searchFunction,
-    );
+  const searchWrapper = (query, searchFunction) => {
+    console.log("searchwrapper")
+    // setState(
+    //   {
+    //     q: query,
+    //     isPreviewOpen: false,
+    //     isAjaxInProgress: true,
+    //     isInlineViewerOpen: false,
+    //     items: [],
+    //     totalResults: 0,
+    //     openedFile: '',
+    //   }
+    // );
+    searchFunction()
   };
 
-  onLoadMore(from, to) {
-    return new Promise((resolve, reject) => {
-      this.setState(
-        {
-          isPreviewOpen: false,
-          isInlineViewerOpen: false,
-          openedFile: '',
-        },
-        () => {
-          resolve(this.search(from, to));
-        },
-      );
-    });
+  const onLoadMore = (from, to) => {
+    // return new Promise((resolve, reject) => {
+    //   this.setState(
+    //     {
+    //       isPreviewOpen: false,
+    //       isInlineViewerOpen: false,
+    //       openedFile: '',
+    //     },
+    //     () => {
+    //       resolve(this.search(from, to));
+    //     },
+    //   );
+    // });
   }
 
-  search(from = 0, to = SIZE) {
-    const query = this.query();
-    if (query) {
-      return new Promise((resolve, reject) => {
+  const search = (from = 0, to = SIZE) => {
+    const theQuery = query();
+    console.log("searching", theQuery, from, to)
+    if (theQuery) {
         axios
-          .get(`${JAWHAR_API}/sources/${this.props.selectSourceType}/${this.props.selectSourceID}/search?q=${query}&from=${from}&to=${to}`)
+          .get(`${JAWHAR_API}/sources/${props.selectSourceType}/${props.selectSourceID}/search?q=${theQuery}&from=${from}&to=${to}`)
           .then(res => {
             const response = res.data;
-            this.setState(
+            setState(
               {
                 totalResults: Math.min(1000, response.length),
-                items: this.state.items.concat(response),
+                items: state.items.concat(response),
                 isAjaxInProgress: false,
-              },
-              () => {
-                resolve(response);
-              },
+              }
             );
           });
-      });
     } else {
-      return new Promise((resolve, reject) => {
-        console.log("Empty Query")
-        this.setState(
-          {
-            totalResults: 0,
-            items: [],
-            emptyQuery: true,
-            isAjaxInProgress: false,
-          },
-          () => {
-            resolve([]);
-          },
-        );
-      })
+      console.log("Empty Query")
+      setState(
+        {
+          totalResults: 0,
+          items: [],
+          emptyQuery: true,
+          isAjaxInProgress: false,
+        }
+      );
     }
   }
 
-  query(fromProps) {
-    const props = fromProps || this.props
-    const parsedQuery = QueryString.parse(props.location.search)
+  const query = () => {
+    console.log("parsing query", location)
+    const parsedQuery = QueryString.parse(location.search)
     return parsedQuery.q
   }
 
-  source(fromProps) {
-    const props = fromProps || this.props
-    return `${this.props.selectSourceType}-${this.props.selectSourceID}`
+  const source = (fromProps) => {
+    const theProps = fromProps || props
+    return `${theProps.selectSourceType}-${theProps.selectSourceID}`
   }
-
-  render() {
-    const { classes } = this.props;
-
-    const { items } = this.state;
-    return (
+  
+  const { items } = state;
+  return (
+    <Root>
       <span>
         <Paper className={classes.filterPaper}>
           <ListItem>
               <Typography className={classes.heading}> Index</Typography>
-              <Select value={this.source()} onChange={(event) => {
+              <Select value={ source()} onChange={(event) => {
                     console.log("Changed source", event.target.value)
                     let c = event.target.value.split("-")
                     let sourceType = c[0]
                     let sourceID = c[1]
-                    this.props.history.push({ pathname: `/search/${sourceType}/${sourceID}`, search: this.props.location.search});
+                    navigate({ pathname: `/search/${sourceType}/${sourceID}`, search: location.search});
               }}>
-                {this.props.sources.filter(s => s.Status == "mounted" && s.IndexStatus != "").map((source, index) => {
+                {props.sources.filter(s => s.Status == "mounted" && s.IndexStatus != "").map((source, index) => {
                   return (
                     <MenuItem value={`${source.Type}-${source.ID}`} >{source.Caption}</MenuItem>)
                   })}
@@ -222,13 +293,14 @@ class Content extends React.Component {
         <Paper className={classes.paper}>
         <div className={classes.contentWrapper} >
           <SearchResultsTable
-            rowCount={this.state.totalResults}
-            rowGetter={({ index }) => ({ file: items[index] })}
-            onPreviewClick={this.handlePreviewClick}
-            onFilenameClick={this.handleFilenameClick}
+            rowCount={state.totalResults}
+            data={state.items}
+            rowGetter={({ index }) => ({ file: state.items[index] })}
+            onPreviewClick={handlePreviewClick}
+            onFilenameClick={handleFilenameClick}
             classes={classes.table}
-            onLoadMore={this.onLoadMore.bind(this)}
-            query={this.query()}
+            onLoadMore={onLoadMore.bind(this)}
+            query={query()}
             columns={[
               {
                 width: 40,
@@ -273,9 +345,10 @@ class Content extends React.Component {
           />
         </div>
       </Paper>
-      <div>{this.state.isAjaxInProgress && <ProgressIndicator />}</div></span>
-    );
-  }
+      <div>{state.isAjaxInProgress && <ProgressIndicator />}</div>
+      </span>
+    </Root>
+  );
 }
 
 Content.propTypes = {
@@ -360,4 +433,4 @@ const deepDiffMapper = (function() {
   };
 })();
 
-export default withRoute(withStyles(styles)(Content));
+export default withRoute(Content);
